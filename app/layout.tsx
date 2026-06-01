6da0e3e1828de847836fd7a2cfa685396287eee9
XVHub <info@xvhub.online>
2026-06-01 21:26:22 +0530

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -58,6 +59,7 @@ export default function RootLayout({
           <Header />
           <main className="flex-1 flex flex-col items-center">{children}</main>
           <Footer />
+          <Analytics />
         </body>
       </html>
     </ClerkProvider>
